8ebaddde6fd689fa2409b9b1e257f8789cee5235
goakiller900 <remco.kappers@gmail.com>
2026-07-11 01:41:35 +0200

    Add files via upload

---
+++ data.lua
@@ -0,0 +1,10 @@
+data:extend({
+  {
+    type = "custom-input",
+    name = "igg-toggle",
+    key_sequence = "SHIFT + ENTER",
+    consuming = "game-only"
+  }
+})
+
+require("prototypes.styles")
---
+++ prototypes/styles.lua
@@ -0,0 +1,6 @@
+data.raw["gui-style"]["default"]["igg-scroll-pane"] =
+{
+  type = "scroll_pane_style",
+  maximal_height = 472,
+  maximal_width = 590
+}
